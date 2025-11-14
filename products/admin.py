d05d406eb761f9f89c02730df3f306510c5ef8d1
ManalAbduqawi <denever_2008@windowslive.com>
2025-11-14 21:51:37 +0000

    setup product app, add models and add products

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from .models import Product, Category, Image
 
 # Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'category',
+        'price',
+        'size',
+        'original_art',
+
+    )
+
+    ordering = ('price',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+class ImageAdmin(admin.ModelAdmin):
+    list_display = (
+        'product',
+        'name',
+        'image',
+        
+    )    
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Image, ImageAdmin)
