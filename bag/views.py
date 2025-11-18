0f2c4d5a0289ae80cc5fdc7925c76709701b959a
ManalAbduqawi <denever_2008@windowslive.com>
2025-11-18 14:20:58 +0000

    add adjust bag quanitity item, delete item from bag and calc subtotal item in bag

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 
 # Create your views here.
 
@@ -21,3 +21,33 @@ def add_to_bag(request, item_id):
 
     request.session['bag'] = bag
     return redirect(redirect_url)
+
+
+def adjust_bag(request, item_id):
+    """ Adjust the quantity of the specified product to the specified amount """
+
+    current_quantity = int(request.POST.get('quantity', 0))
+    bag = request.session.get('bag', {})
+
+
+        # Enforce min/max limits
+    new_quantity = max(1, min(99,  current_quantity))
+
+    if new_quantity > 0:
+        bag[item_id] = new_quantity
+    else:
+        bag.pop(item_id, None)
+
+    request.session['bag'] = bag
+    return redirect('view_bag')
+  
+
+def remove_from_bag(request, item_id):
+    """ Remove the item from the shopping bag """
+    bag = request.session.get('bag', {})
+
+
+    bag.pop(str(item_id), None)
+
+    request.session['bag'] = bag
+    return redirect('view_bag')
