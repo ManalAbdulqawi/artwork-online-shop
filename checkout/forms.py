2fb69d997ca97e0997a278ffcda4993a32017ec0
ManalAbduqawi <denever_2008@windowslive.com>
2025-11-24 01:39:51 +0000

    Update delivery banner and make the country field in checkout hidden and readonly with UK option

--- checkout/forms.py
+++ checkout/forms.py
@@ -16,6 +16,15 @@ class OrderForm(forms.ModelForm):
         labels and set autofocus on first field
         """
         super().__init__(*args, **kwargs)
+        # Set the initial value for 'country'
+        self.fields['country'].initial = 'UK'
+        # Make the field read-only and style it
+        self.fields['country'].widget.attrs['readonly'] = True
+        self.fields['country'].widget.attrs['style'] = 'background-color: #eee;'
+        # Make sure it's disabled so user can't change it
+        self.fields['country'].widget.attrs['disabled'] = True
+        # Replace widget with HiddenInput to submit the value
+        self.fields['country'].widget = forms.HiddenInput()
         placeholders = {
             'full_name': 'Full Name',
             'email': 'Email Address',
