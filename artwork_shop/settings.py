5e9a0aa8e17c0e7b893b29c17e4e78ffc8ef7b68
ManalAbduqawi <denever_2008@windowslive.com>
2025-11-28 10:54:16 +0000

    make debug true to see the cause of error

--- artwork_shop/settings.py
+++ artwork_shop/settings.py
@@ -29,7 +29,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
+#'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['127.0.0.1', # local host
                   'localhost', # listen for stripe webhooks
