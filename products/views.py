4f394debe78ba4fcbaa50cdd2aa455eef5223da5
ManalAbduqawi <denever_2008@windowslive.com>
2025-12-01 02:57:18 +0000

    add watchlist feature with its model, template and views

--- products/views.py
+++ products/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
 from django.db.models.functions import Lower 
-from .models import Product, Category
+from .models import Product, Category, Watchlist
+from django.contrib.auth.decorators import login_required
+
+
 
 # Create your views here.
 
@@ -63,12 +66,42 @@ def product_detail(request, product_id):
     product = get_object_or_404(Product, pk=product_id)
     images = product.images.all()
     images_count = product.images.count()
+    user_watched_products = []
+    if request.user.is_authenticated:
+        user_watched_products = request.user.watchlist.values_list('product_id', flat=True)
 
     context = {
         'product': product,
         'images': images,
         'images_count': images_count,
+        'user_watched_products': list(user_watched_products),
 
     }
 
     return render(request, 'products/product_detail.html', context)
+
+
+@login_required
+def watchlist_view(request):
+    watchlist_items = request.user.watchlist.select_related('product')
+    return render(request, 'products/watchlist.html', {'watchlist_items': watchlist_items})
+
+
+
+@login_required
+def add_to_watchlist(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    watchlist_item, created = Watchlist.objects.get_or_create(user=request.user, product=product)
+    if created:
+        messages.success(request, 'Product added to your watchlist.')
+    else:
+        messages.info(request, 'Product is already in your watchlist.')
+    return redirect('product_detail', product_id=product.id)
+
+
+@login_required
+def remove_from_watchlist(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    Watchlist.objects.filter(user=request.user, product=product).delete()
+    messages.success(request, 'Product removed from your watchlist.')
+    return redirect('watchlist')
